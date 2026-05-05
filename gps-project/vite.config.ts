831b7e5a44224937fdc5227fdf63bfddf22d7240
Bram Martens <brammartens@live.nl>
2026-05-05 17:22:20 +1200

    pipeline fix

--- gps-project/vite.config.ts
+++ gps-project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiBase = env.VITE_API_BASE_URL || 'https://bramcloud.nl'
+  const apiBase = env.VITE_API_BASE_URL || 'http://host.docker.internal:5001'
 
   return {
     plugins: [react()],
